e74f1a943dd60a0a91cbcd09e93967e037365143
codecorneres <codecorneres@gmail.com>
2019-07-23 17:55:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { createBrowserHistory } from 'history';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Landing from './component/Landing'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const history = createBrowserHistory();
+    return (
+      <div className="App">
+        <Router history={history}>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+          </Switch>
+        </Router>
+      </div>
+    );
 }
 
 export default App;
